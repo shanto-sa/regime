a75b8cf18e279155d9713298fc588e37583be5cf
Proshanta Halder <p.haider@seu.edu.sa>
2024-07-29 10:52:11 +0300

    Profile Update Screen Language Update

--- Screen/EditInfoScreen.js
+++ Screen/EditInfoScreen.js
@@ -7,6 +7,7 @@ const EditInfoScreen = () => {
   const windowWidth = Dimensions.get('window').width;
   const dispatch = useDispatch();
   const { loading, error } = useSelector(state => state.userProfile);
+  const { strings } = useSelector((state) => state.commonData);
 
   const [selectedGender, setSelectedGender] = useState(null);
   const [name, setName] = useState('');
@@ -39,25 +40,25 @@ const EditInfoScreen = () => {
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
         <View style={{ flex: 1, padding: 20 }}>
           <Text style={{ fontSize: 24, color:'#000000', fontWeight: 'bold', textAlign: 'center', marginBottom: 16 }}>
-            تعديل البروفايل
+             {strings.edit_profile}
           </Text>
           <View style={{ alignItems: 'center', marginBottom: 30, padding:15, backgroundColor: '#ffffff', borderRadius:15 }}>
             <Image source={require('../Image/profile.png')} style={{ width: 90, height: 90, borderRadius: 60, marginBottom: 8 }} />
-            <TextInput placeholder="الاسم" value={name} onChangeText={setName} style={{ borderWidth: 1, borderColor: '#CCCCCC', borderRadius: 5, paddingHorizontal: 10, marginBottom: 8, width: windowWidth - 70 }} />
-            <TextInput placeholder="رقم الهاتف" value={phoneNumber} onChangeText={setPhoneNumber} style={{ borderWidth: 1, borderColor: '#CCCCCC', borderRadius: 5, paddingHorizontal: 10, marginBottom: 8, width: windowWidth - 70 }} />
-            <TextInput placeholder="الطول" value={height} onChangeText={setHeight} style={{ borderWidth: 1, borderColor: '#CCCCCC', borderRadius: 5, paddingHorizontal: 10, marginBottom: 8, width: windowWidth - 70 }} />
-            <TextInput placeholder="الوزن" value={weight} onChangeText={setWeight} style={{ borderWidth: 1, borderColor: '#CCCCCC', borderRadius: 5, paddingHorizontal: 10, marginBottom: 8, width: windowWidth - 70 }} />
-            <TextInput placeholder="مستوى النشاط" value={activityLevel} onChangeText={setActivityLevel} style={{ borderWidth: 1, borderColor: '#CCCCCC', borderRadius: 5, paddingHorizontal: 10, marginBottom: 8, width: windowWidth - 70 }} />
-            <TextInput placeholder="الهدف" value={goal} onChangeText={setGoal} style={{ borderWidth: 1, borderColor: '#CCCCCC', borderRadius: 5, paddingHorizontal: 10, marginBottom: 8, width: windowWidth - 70 }} />
+            <TextInput placeholder={strings.name} value={name} onChangeText={setName} style={{ borderWidth: 1, borderColor: '#CCCCCC', borderRadius: 5, paddingHorizontal: 10, marginBottom: 8, width: windowWidth - 70 }} />
+            <TextInput placeholder={strings.phone_number} value={phoneNumber} onChangeText={setPhoneNumber} style={{ borderWidth: 1, borderColor: '#CCCCCC', borderRadius: 5, paddingHorizontal: 10, marginBottom: 8, width: windowWidth - 70 }} />
+            <TextInput placeholder={strings.height} value={height} onChangeText={setHeight} style={{ borderWidth: 1, borderColor: '#CCCCCC', borderRadius: 5, paddingHorizontal: 10, marginBottom: 8, width: windowWidth - 70 }} />
+            <TextInput placeholder={strings.weight} value={weight} onChangeText={setWeight} style={{ borderWidth: 1, borderColor: '#CCCCCC', borderRadius: 5, paddingHorizontal: 10, marginBottom: 8, width: windowWidth - 70 }} />
+            <TextInput placeholder={strings.activity_level} value={activityLevel} onChangeText={setActivityLevel} style={{ borderWidth: 1, borderColor: '#CCCCCC', borderRadius: 5, paddingHorizontal: 10, marginBottom: 8, width: windowWidth - 70 }} />
+            <TextInput placeholder={strings.goal} value={goal} onChangeText={setGoal} style={{ borderWidth: 1, borderColor: '#CCCCCC', borderRadius: 5, paddingHorizontal: 10, marginBottom: 8, width: windowWidth - 70 }} />
 
             <View style={{ flexDirection: 'row', width: '100%', marginBottom: 20 }}>
               <TouchableOpacity onPress={() => handleGenderSelection('male')} style={{ flexDirection: 'row', justifyContent: 'space-between',  backgroundColor:'#ffffff', borderWidth: 1, borderColor: '#CCCCCC',  borderRadius:15, padding:10, alignItems: 'center', marginRight: 5, flex: 1 }}>
                 <View style={{ width: 20, height: 20, borderRadius: 10, backgroundColor: selectedGender === 'male' ? '#006F34' : '#CCCCCC', marginRight: 5 }} />
-                <Text style={{ color: 'black' }}>ذكر</Text>
+                <Text style={{ color: 'black' }}>{strings.male}</Text>
               </TouchableOpacity>
               <TouchableOpacity onPress={() => handleGenderSelection('female')} style={{ flexDirection: 'row', justifyContent: 'space-between',  borderWidth: 1, borderColor: '#CCCCCC',  backgroundColor:'#ffffff', borderRadius:15, padding:10, alignItems: 'center', marginLeft: 5, flex: 1 }}>
                 <View style={{ width: 20, height: 20, borderRadius: 10, backgroundColor: selectedGender === 'female' ? '#006F34' : '#CCCCCC', marginRight: 5 }} />
-                <Text style={{ color: 'black', }}>أنثى</Text>
+                <Text style={{ color: 'black', }}>{strings.female}</Text>
               </TouchableOpacity>
             </View>
 
@@ -73,7 +74,7 @@ const EditInfoScreen = () => {
               disabled={loading}
             >
               <Text style={{ color: 'white', fontWeight: 'bold', textAlign: 'center', fontSize: 18 }}>
-                {loading ? 'جاري الحفظ...' : 'حفظ'}
+              {loading ? strings.saving : strings.save}
               </Text>
             </TouchableOpacity>
 
